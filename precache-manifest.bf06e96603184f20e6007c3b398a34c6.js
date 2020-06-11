df36835dfe2503735a78217bad110368359e6e91
mac <mac@macs-MacBook-Pro.local>
2020-06-11 13:01:02 +0200

    Updates

---
+++ precache-manifest.bf06e96603184f20e6007c3b398a34c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cc46b60e1fdf88ab4a13b4513df79d6",
+    "url": "/Pokemon/index.html"
+  },
+  {
+    "revision": "1c439ebe6376f383cf0d",
+    "url": "/Pokemon/static/css/main.0857bba3.chunk.css"
+  },
+  {
+    "revision": "4dd5f7954936c7a30049",
+    "url": "/Pokemon/static/js/2.d801ffa5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Pokemon/static/js/2.d801ffa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c439ebe6376f383cf0d",
+    "url": "/Pokemon/static/js/main.716e2783.chunk.js"
+  },
+  {
+    "revision": "9dbf7c6c2a20ff885131",
+    "url": "/Pokemon/static/js/runtime-main.5def3f89.js"
+  }
+]);
